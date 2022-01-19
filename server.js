39b9ce6f6391bb2541c182ffc4fadafae7b19a41
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-01-20 00:06:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -15,8 +17,8 @@ app.use(express.json());
 const userRouter = require('./routes/user');
 app.use('/user', userRouter);
 
-app.listen (3000, () => {
-  console.log('listening on port 3000');
+app.listen (PORT, () => {
+  console.log('listening on port', PORT);
 }
 );
 
