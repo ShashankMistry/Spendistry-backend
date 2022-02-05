06b986902b4d9ef78b4b603b532801a550637afc
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-02-05 21:32:24 +0530

    Update ItemPricesList.js

--- routes/ItemPricesList.js
+++ routes/ItemPricesList.js
@@ -19,10 +19,11 @@ router.get('/:id', getItems, (req, res) => {
     res.json(res.item);
 })
 
-router.get('/filter/:id', async (req, res) => {
+router.get('/filter/:id/:ItemsPrices', async (req, res) => {
     // res.send(`getting user by invoiceSentTo ${req.params.invoiceSentTo}`);
     try {
-    const itemsPrices = await ItemPricesSchema.find({id: req.params.id});
+    // const itemsPrices = await ItemPricesSchema.find({"_id": req.params.id, "ItemsPrices": {$elemMatch: { "price": req.params.ItemsPrices }}});
+    const itemsPrices = await ItemPricesSchema.aggregate([{$match: {"_id": req.params.id}}, {$unwind: "$ItemsPrices"}, {$match: {"ItemsPrices.price": req.params.ItemsPrices}}]);
     res.json(itemsPrices);
     } catch (err) {
         res.status(500).json({message: err.message});
