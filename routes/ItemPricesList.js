ddf44951fc46ad1f90124f20dfc6b99460f41b58
Shashank Mistry <shashankmistry30@gmail.com>
2022-02-05 16:39:05 +0530

    Update ItemPricesList.js

--- routes/ItemPricesList.js
+++ routes/ItemPricesList.js
@@ -19,10 +19,10 @@ router.get('/:id', getItems, (req, res) => {
     res.json(res.item);
 })
 
-router.get('/filter/:invoiceSentTo/:price', async (req, res) => {
+router.get('/filter/:invoiceSentTo', async (req, res) => {
     // res.send(`getting user by invoiceSentTo ${req.params.invoiceSentTo}`);
     try {
-    const itemsPrices = await ItemPricesSchema.find({invoiceSentTo: req.params._id , price: req.params.ItemsPrices.price});
+    const itemsPrices = await ItemPricesSchema.find({invoiceSentTo: req.params._id});
     res.json(invoice);
     } catch (err) {
         res.status(500).json({message: err.message});
