e4940fc995007b0ee30f496f48e2bd05399f03f1
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-02-16 13:42:13 +0530

    Update invoice.js

--- models/invoice.js
+++ models/invoice.js
@@ -1,94 +1,111 @@
+const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 
 const invoiceSchema = new mongoose.Schema({
-    invoiceNumber: {
+    _id: {
         type: String
-       
     },
-    invoiceDate: {
-        type: String
+    invoices: {
     
-    },
-    invoiceAmount: {
-        type: String
-        
-    },
-    invoiceStatus: {
-        type: String
-        
-    },
-    invoiceTitle: {
-        type: String
-      
-    },
-    invoiceTotalitems: {
-        type: String
-        
-    },
-    invoiceIGST: {
-        type: String
-      
-    },
-    invoiceCGST: {
-        type: String
-       
-    },
-    invoiceSGST: {
-        type: String
-   
-    },
-    invoiceUTGST: {
-        type: String
-    
-    },
-    invoiceSentTo: {
-        type: String
-      
-    },
-    invoiceSentBy: {
-        type: String
-      
-    },
-    invoicePaymentMode: {
-        type: String
-     
-    },
-    invoicePDF: {
-        type: String
-     
-    },
-    invoiceReport: {
-        type: String
-   
-    },
-    invoiceTime : {
-        type: Date,
-        default: Date.now
-    },
-    discount : {
-        type: String
-    },
-    roundoff : {
-        type: String
-    },
-    city : {
-        type: String
-    },
-    extra1: {
-        type: String
-    },
-    extra2: {
-        type: String
-    },
-    extra3: {
-        type: String
-    },
-    extra4: {
-        type: String
-    },
-    extra5: {
-        type: String
-    }
+    type: [
+        {
+            invoiceNumber: {
+                type: String
+               
+            },
+            invoiceDate: {
+                type: String
+            
+            },
+            invoiceAmount: {
+                type: String
+                
+            },
+            invoiceStatus: {
+                type: String
+                
+            },
+            invoiceTitle: {
+                type: String
+              
+            },
+            invoiceTotalitems: {
+                type: [{
+                    itemName: {
+                        type: String
+                    },
+                    itemPrice: {
+                        type: String
+                    }
+                }]
+            },
+            invoiceIGST: {
+                type: String
+              
+            },
+            invoiceCGST: {
+                type: String
+               
+            },
+            invoiceSGST: {
+                type: String
+           
+            },
+            invoiceUTGST: {
+                type: String
+            
+            },
+            invoiceSentTo: {
+                type: String
+              
+            },
+            invoiceSentBy: {
+                type: String
+              
+            },
+            invoicePaymentMode: {
+                type: String
+             
+            },
+            invoicePDF: {
+                type: String
+             
+            },
+            invoiceReport: {
+                type: String
+           
+            },
+            invoiceTime : {
+                type: Date,
+                default: Date.now
+            },
+            discount : {
+                type: String
+            },
+            roundoff : {
+                type: String
+            },
+            city : {
+                type: String
+            },
+            extra1: {
+                type: String
+            },
+            extra2: {
+                type: String
+            },
+            extra3: {
+                type: String
+            },
+            extra4: {
+                type: String
+            },
+            extra5: {
+                type: String
+            }
+        }
+    ]
+}
 });
 
 module.exports = mongoose.model('Invoice', invoiceSchema);
