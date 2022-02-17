1ee2c63812e9a614088ac961d77bbfaafbde8627
Shashank Mistry <shashankmistry30@gmail.com>
2022-02-18 02:47:05 +0530

    Update ItemPricesList.js

--- routes/ItemPricesList.js
+++ routes/ItemPricesList.js
@@ -121,7 +121,7 @@ router.patch('/addItems/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json({message: err.message});
     }
-    console.log(items.ItemsPrices[item.ItemsPrices.length - 1]._id);
+    console.log(items.ItemsPrices[items.ItemsPrices.length - 1]._id);
 })
 
 // updating one
