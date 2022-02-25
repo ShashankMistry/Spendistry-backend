8bc6d0141070bbe2dcf8daa9c4a426c9598a9911
Shashank Mistry <shashankmistry30@gmail.com>
2022-02-25 17:09:29 +0530

    Update ItemPricesList.js

--- routes/ItemPricesList.js
+++ routes/ItemPricesList.js
@@ -102,7 +102,7 @@ router.post('/', async (req, res) => {
     try{
         const savedItems = await item.save();
         res.status(201).json(savedItems);
-        
+        console.log(ItemsPrices)
     }
     catch (err) {
         res.status(400).json({message: err.message});
