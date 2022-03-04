f79e83eb83b0e62641b7ee70b598bde7bc442154
Shashank Mistry <shashankmistry30@gmail.com>
2022-03-04 21:50:24 +0530

    Update authBusiness.js

--- routes/authBusiness.js
+++ routes/authBusiness.js
@@ -77,7 +77,7 @@ router.post('/vendorLogin', async (req, res) => {
         res.header('Access-Control-Allow-Credentials', 'true');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        // res.header('auth-token-vendor', token).send(token);
+        res.header('auth-token-vendor', token).send(token);
         console.log(vendor);
         // res.json(vendor);
         // res.status(200).json({message: 'Successfully logged in'});
