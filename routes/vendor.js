7ac577e7514be4878f15cecd4c83543143e9794a
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-07 15:26:25 +0530

    Update vendor.js

--- routes/vendor.js
+++ routes/vendor.js
@@ -68,7 +68,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage, limits: {fileSize: 10}});
 
-app.post('/upload', upload.single('vendorProfile'), (req, res) => {
+router.post('/upload', upload.single('vendorProfile'), (req, res) => {
     // res.send(req.file);
     try {
         res.send(req.file);
