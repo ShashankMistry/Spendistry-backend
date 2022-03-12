eee2203cec0ca5a5986ecac1b9bad65744aef4a2
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-12 17:19:23 +0530

    Update mvd.js

--- routes/mvd.js
+++ routes/mvd.js
@@ -67,7 +67,7 @@ router.get('/:id', async (req, res) => {
         //get the vendor name
 
         {$lookup: {
-            from: 'Vendor',
+            from: 'vendor',
             localField: 'businessName._id',
             foreignField: '_id',
             as: 'vendor'
