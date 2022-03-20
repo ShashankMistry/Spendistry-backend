31c7010e0b1c7fce92303fc13497b49e9e840a8e
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-20 18:00:53 +0530

    Update mvd.js

--- routes/mvd.js
+++ routes/mvd.js
@@ -58,8 +58,22 @@ router.get('/:id', async (req, res) => {
                         $push: '$businessName.invoices'
                     },
                     roundoff:{
-                        $push:'$businessName.invoices.roundoff'
-                    }
+                        // $push:'$businessName.invoices.roundoff'
+                        $cond: {
+                            if: {
+                                $gte: [
+                                    '$businessName.invoices.invoiceTime',
+                                    Date.now - (1000 * 60 * 60 * 24 * 30)
+                                ],
+                            },
+                                // then: '$businessName.invoices.roundoff',
+                                then: {
+                                    $push: '$businessName.invoices.roundoff'
+                                },
+                                else: 0
+                             }
+
+                         }
     
                 }
             },
