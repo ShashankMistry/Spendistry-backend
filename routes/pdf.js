db25a2bec88eaf093967473a300abb3368bde596
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-22 16:01:43 +0530

    Update pdf.js

--- routes/pdf.js
+++ routes/pdf.js
@@ -2,37 +2,29 @@ const express = require('express');
 const router = express.Router();
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
-const multer = require('multer');
 
 const doc = new PDFDocument();
 
-//setting up storage to save pdf
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './upload/pdf');
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname);
-    }
-});
 
-//store a pdf file
-const upload = multer({ storage: storage });
-
-//create a pdf
-router.post('/pdf', upload.single('pdf'), (req, res) => {
-
-});
-
-
-//create a new PDF file
+//get a new PDF file
 // router.get('/', (req, res) => {
-//     doc.pipe(fs.createWriteStream('invoice.pdf'));
-//     doc.pipe(res);
-//     doc.text('Hello World1');
-//     doc.end();
-//     res.download('invoice.pdf');
+//     res.download('upload/pdf/invoice.pdf');
 // });
+
+//create a new PDF file inside upload/pdf folder
+router.get('/', async (req, res) => {
+    doc.pipe(fs.createWriteStream('upload/pdf/invoice.pdf'));
+    doc.pipe(res);
+    doc.text('Hello Om');
+    doc.end();
+    res.download('upload/pdf/invoice.pdf');
+    // delete pdf file after download
+    fs.unlink('upload/pdf/invoice.pdf', (err) => {
+        if (err) throw err;
+        console.log('successfully deleted');
+    })
+});
+
 
 
 module.exports = router;
