64e43e1b0d7b5d09701b5c443ca0261f6ede2262
Shashank Mistry <shashankmistry30@gmail.com>
2022-03-23 15:34:41 +0530

    Update invoice.js

--- routes/invoice.js
+++ routes/invoice.js
@@ -535,7 +535,7 @@ router.patch('/patchEle/:userid/:vendorid/:invoiceid', async (req, res) => {
             {
                 $set: {
                     "businessName.$[d].invoices.$[o].invoiceAmount": req.body.invoices.invoiceAmount,
-                    "businessName.$[d].invoices.$[o].invoiceTime": Date.now,
+                    "businessName.$[d].invoices.$[o].invoiceTime": Date.now(),
                     "businessName.$[d].invoice.$[o].roundoff": req.body.invoices.roundoff,
                     "businessName.$[d].invoices.$[o].discount": req.body.invoices.discount,
                     "businessName.$[d].invoices.$[o].invoiceTotalitems": req.body.invoices.invoiceTotalitems,
