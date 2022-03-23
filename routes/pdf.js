5a6b7724c9fb25f01508aec259cf2edbd3d19a1f
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-23 14:05:40 +0530

    Update pdf.js

--- routes/pdf.js
+++ routes/pdf.js
@@ -60,7 +60,10 @@ router.get('/:userId/:vendorId/:invoiceId', async (req, res) => {
         ]).then(invoice => {
         
         // calling the function to create a pdf file
-        const doc = new PDFDocument();
+        const doc = new PDFDocument({
+            size: 'A4',
+            margin: 50
+        });
         res.setHeader('Content-disposition', 'attachment; filename='+req.params.vendorId+"_"+Date.now()+'.pdf');
         doc.pipe(res);
 
@@ -68,35 +71,92 @@ router.get('/:userId/:vendorId/:invoiceId', async (req, res) => {
         doc.info['Title'] = 'Invoice';
         doc.info['Author'] = req.params.vendorId;
         doc.info['Subject'] = 'Invoice';
-        
 
-
-        doc.text('Hello Om');
-        doc.text('Invoice Number: '+invoice[0].invoices.invoiceNumber);
-        doc.text('reason'+invoice[0].invoices.reportReason);
-
-        // doc.text(invoice[0].businessName.invoices.invoiceDate);
-        doc.text(invoice[0].invoices.invoiceSentBy);
-        doc.text(invoice[0].invoices.invoiceSentTo);
-        doc.text(invoice[0].invoices.invoiceTitle);
-        doc.text(invoice[0].invoices.invoiceTime);
-        // doc.text(invoice[0].businessName.invoices.customerNumber);
-        // doc.text(invoice[0].businessName.invoices.customerName);
-        // doc.text(invoice[0].businessName.invoices.businessName);
-        doc.text(invoice[0].invoices.businessContactNo);
-        // doc.text(invoice[0].businessName.invoices.reportReason);
-        // doc.text(invoice[0].businessName.invoices.InvoiceID);
-        doc.text(invoice[0].invoices.invoiceAmount);
-        doc.text(invoice[0].invoices.invoiceIGST);
-        doc.text(invoice[0].invoices.invoiceCGST);
-        doc.text(invoice[0].invoices.invoiceSGST);
-        invoice[0].invoices.invoiceTotalitems.map(item => {
-            doc.text("name: "+item.itemName);
-            doc.text("qnt"+item.quantity);
-            doc.text("price"+item.price);
-            doc.text("total"+item.total);
-
+        //title of the pdf file
+        doc.fontSize(25).text(invoice[0].invoices.invoiceTitle, {
+            underline: true
         });
+
+        //business address
+        doc.fontSize(12).text(invoice[0].invoices.businessAddress);
+
+        
+      //city in upper case
+      const city = invoice[0].invoices.city.toUpperCase();
+
+        //subject to jurisdiction
+        doc.fontSize(12).text('SUBJECT TO'+city+'JURISDICTION ONLY');
+
+        //GST number
+        doc.fontSize(12).text('GST NO:'+invoice[0].invoices.gstNumber);
+
+        //vendor id
+        doc.fontSize(12).text('VENDOR ID:'+req.params.vendorId);
+
+        //business contact number
+        doc.fontSize(12).text('CONTACT NO:'+invoice[0].invoices.businessContactNo);
+
+        //convert unix time to date
+        const date = new Date(invoice[0].invoices.invoiceTime);
+        const dateString = date.toDateString();
+
+
+        //invoice date and time
+        doc.fontSize(12).text('INVOICE DATE:'+dateString);
+
+        //user ID 
+        doc.fontSize(12).text('USER ID:'+req.params.userId);
+
+        //Invoice number
+        doc.fontSize(12).text('INVOICE NO:'+invoice[0].invoices.invoiceNumber);
+
+        //create a table of invoiceTotalItems
+        doc.fontSize(12).table().unstyled().body([
+            ['ITEM', 'QUANTITY', 'UNIT PRICE', 'AMOUNT']
+        ])
+
+        //create a table of invoiceTotalItems
+        doc.fontSize(12).table().unstyled().body(invoice[0].invoices.invoiceTotalItems.map(item => [
+            item.itemName,
+            item.quantity,
+            item.price,
+            item.total
+        ]));
+
+        //total amount
+        doc.fontSize(12).text('TOTAL AMOUNT:'+invoice[0].invoices.invoiceAmount);
+
+        //all kind of gst 
+        doc.fontSize(12).text('IGST'+invoice[0].invoices.invoiceIGST);
+        doc.fontSize(12).text('CGST'+invoice[0].invoices.invoiceCGST);
+        doc.fontSize(12).text('SGST'+invoice[0].invoices.invoiceSGST);
+        doc.fontSize(12).text('UTGST'+invoice[0].invoices.invoiceUTGST);
+
+        //discount
+        doc.fontSize(12).text('DISCOUNT:'+invoice[0].invoices.discount);
+
+        //payment method
+        doc.fontSize(12).text('PAYMENT METHOD:'+invoice[0].invoices.invoicePaymentMode);
+
+        //net total (roundoff)
+        doc.fontSize(12).text('NET TOTAL:'+invoice[0].invoices.roundoff);
+        
+        //invoice description
+        doc.fontSize(12).text('INVOICE DESCRIPTION:'+invoice[0].invoices.invoiceDescription);
+
+
+        // doc.text('Hello Om');
+        // doc.text('Invoice Number: '+invoice[0].invoices.invoiceNumber);
+        // doc.text('reason'+invoice[0].invoices.reportReason);
+
+    
+        // invoice[0].invoices.invoiceTotalitems.map(item => {
+        //     doc.text("name: "+item.itemName);
+        //     doc.text("qnt"+item.quantity);
+        //     doc.text("price"+item.price);
+        //     doc.text("total"+item.total);
+
+        // });
 
         doc.end();
 
