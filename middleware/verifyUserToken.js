556e8f30869471b98bbf7243705c0dd2b5ce57d1
Shashank Mistry <shashankmistry30@gmail.com>
2022-03-28 23:27:34 +0530

    Update verifyUserToken.js

--- middleware/verifyUserToken.js
+++ middleware/verifyUserToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next){
     const token = req.header('auth-token');
-    console.log(req.header);
+    console.log(req.headers);
     if (!token) {
         return res.status(401).send('Access denied. No token provided.');
     }
