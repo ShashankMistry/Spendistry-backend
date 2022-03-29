2dcadd47db1e1ab47d6414025a1438a8ffa022e8
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-29 16:52:10 +0530

    Update mud.js

--- routes/mud.js
+++ routes/mud.js
@@ -4,7 +4,64 @@ const invoice = require('../models/invoice');
 const mongoose = require('mongoose');
 const report = require('../models/report');
 const user = require('../models/user');
+const cryptoJS = require('crypto-js');
 
+//get user dashboard
+router.get('/:id', async (req, res) => {
 
+     //hash the id with cryptoJS
+     var encryptedQr = cryptoJS.AES.encrypt(req.params.id, process.env.QR_HASH_KEY).toString();
+
+    try {
+    const total = await Invoice.aggregate([
+        {$match: { _id: req.params.id}
+        },
+        {$unwind: '$businessName'},    
+        {$unwind: '$businessName.invoices'},
+        {$group: {
+            _id: req.params.id,
+            MonthlyTotalAll: {
+                $sum : {
+                       $cond: {
+                         if: {
+                             $gte: [
+                                 '$businessName.invoices.invoiceTime',
+                                 Date.now - (1000 * 60 * 60 * 24 * 30)
+                             ],  
+                         },
+                         then: '$businessName.invoices.roundoff',
+                         else: 0
+                     }
+                 }
+             },
+             AllTimeTotal:{
+                 $sum:'$businessName.invoices.roundoff'
+             },
+           
+        }},
+        {$unwind: '$business'},
+        {$group: {      
+            _id:'$business._id',
+            MonthlyTotalAll: {$last: '$MonthlyTotalAll'},
+            AllTimeTotal:{$last: '$AllTimeTotal'},
+        }
+    },
+        
+        {$project: {
+            _id: '$_id',
+            MonthlyTotalAll: '$MonthlyTotalAll',
+            AllTimeTotal:  '$AllTimeTotal',
+            qr: encryptedQr
+        }
+    },
+
+    ]);
+    res.send(total);
+    
+        
+    } catch (error) {
+        
+    }
+});
 
 exports.default = router;
