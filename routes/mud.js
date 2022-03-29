ddeab0610ec2903963a35e972c1a5c40436fc68a
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-29 17:04:28 +0530

    Update mud.js

--- routes/mud.js
+++ routes/mud.js
@@ -13,49 +13,22 @@ router.get('/:id', async (req, res) => {
      var encryptedQr = cryptoJS.AES.encrypt(req.params.id, process.env.QR_HASH_KEY).toString();
 
     try {
-    const total = await Invoice.aggregate([
-        {$match: { _id: req.params.id}
-        },
-        {$unwind: '$businessName'},    
-        {$unwind: '$businessName.invoices'},
-        {$group: {
-            _id: req.params.id,
-            MonthlyTotalAll: {
-                $sum : {
-                       $cond: {
-                         if: {
-                             $gte: [
-                                 '$businessName.invoices.invoiceTime',
-                                 Date.now - (1000 * 60 * 60 * 24 * 30)
-                             ],  
-                         },
-                         then: '$businessName.invoices.roundoff',
-                         else: 0
-                     }
-                 }
-             },
-             AllTimeTotal:{
-                 $sum:'$businessName.invoices.roundoff'
-             },
-           
-        }},
-        {$group: {      
-            MonthlyTotalAll: {$last: '$MonthlyTotalAll'},
-            AllTimeTotal:{$last: '$AllTimeTotal'},
-        }
-    },
-        
-        {$project: {
-            MonthlyTotalAll: '$MonthlyTotalAll',
-            AllTimeTotal:  '$AllTimeTotal',
+        const invoices = await Invoice.find({
+            userId: req.params.id
+        });
+        const reports = await report.find({
+            userId: req.params.id
+        });
+        const users = await user.find({
+            _id: req.params.id
+        });
+        res.render('dashboard', {
+            invoices: invoices,
+            reports: reports,
+            users: users,
             qr: encryptedQr
-        }
-    },
-
-    ]);
-    res.send(total);
-    
-        
+        });
+                
     } catch (error) {
         res.status(500).send(error);
         
