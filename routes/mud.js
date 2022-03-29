be97f6384ddbaa5f3d8f788713f720da059434f8
Om Brahmbhatt <om.brahmbhatt123@gmail.com>
2022-03-29 16:56:59 +0530

    Update mud.js

--- routes/mud.js
+++ routes/mud.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const invoice = require('../models/invoice');
+const Invoice = require('../models/invoice');
 const mongoose = require('mongoose');
 const report = require('../models/report');
 const user = require('../models/user');
